52241d155598051ee12075e4c5b2e20de77bbebd
Panayot Cankov <Panayot.Cankov@telerik.com>
2016-05-10 12:20:59 +0300

    Somewhat iOS

--- index.ts
+++ index.ts
@@ -1,14 +1,16 @@
+/// <reference path="./ios/objc!UIKit.d.ts" />
+
 declare var isAndroid: boolean;
 declare var isIOS: boolean;
 
 class Button {
-    ios: UIButton;
-    android: android.widget.Button;
+    ios: any;
+    //android: android.widget.Button;
     constructor() {
         if (isAndroid) {
-            this.android = new android.widget.Button(null);
+            //this.android = new android.widget.Button(null);
         } else if (isIOS) {
-            this.ios = new UIButton();
+            this.ios = UIButton.alloc();// .init();
         }
     }
 }
